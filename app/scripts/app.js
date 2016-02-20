7e55c211769ad973c84eeafb3abd37eff3de0ca9
Koding <rajatnigam@rajatnigam.koding.io>
2016-02-20 03:40:32 -0600

    Project Setup

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name script2bitApp
+ * @description
+ * # script2bitApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('script2bitApp', [
+    'ngAria',
+    'ngMessages',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name script2bitApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the script2bitApp
+ */
+angular.module('script2bitApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
